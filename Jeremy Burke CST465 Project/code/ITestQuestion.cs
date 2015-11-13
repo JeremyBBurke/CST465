451e2516c9cd407ac91863cac0e4f479600eda60
STUDENTS\jeremy.burke <jeremybcool@gmail.com>
2015-11-12 18:52:06 -0800

    Why were these untracked?!?!?

---
+++ Jeremy Burke CST465 Project/code/ITestQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeremy_Burke_CST465_Project.Code
+{
+    public interface ITestQuestion
+    {
+        string QuestionText
+        {
+            get;
+            set;
+        }
+        string Answer
+        {
+            get;
+            set;
+        }
+    }
+}
